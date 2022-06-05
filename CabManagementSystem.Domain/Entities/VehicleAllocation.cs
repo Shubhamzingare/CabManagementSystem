ef40909513dae82693741547ac57d9d03c47490d
n-singam <n.singam@zensar.com>
2022-06-05 20:24:56 +0530

    added vehicleallocation.cs

---
+++ CabManagementSystem.Domain/Entities/VehicleAllocation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabManagementSystem.Domain.Entities
+{
+    public class VehicleAllocation
+    {
+        public string vehicleAlloId { get; set; }
+        public string vehicleId { get; set; }
+        public string empId { get; set; }
+        public string driverId { get; set; }
+        public string pickOrDrop { get; set; }
+        public string routeId { get; set; }
+    }
+}
