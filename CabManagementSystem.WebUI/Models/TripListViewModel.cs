134a125b80080c44b29fb5f858966e74417eaaf1
n-singam <n.singam@zensar.com>
2022-06-06 09:05:59 +0530

    -

---
+++ CabManagementSystem.WebUI/Models/TripListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CabManagementSystem.Domain.Entities;
+
+namespace CabManagementSystem.WebUI.Models
+{
+    public class TripListViewModel
+    {
+        public IEnumerable<Trip> TripSheet { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
