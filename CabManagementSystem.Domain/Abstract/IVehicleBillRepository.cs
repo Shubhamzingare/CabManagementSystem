c7ceca411d3c95e06a4f9a444e78c28dd13eb7fe
Shubhamzingare123 <s.zingare@zensar.com>
2022-06-08 15:36:16 +0530

    repo entity interface

---
+++ CabManagementSystem.Domain/Abstract/IVehicleBillRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabManagementSystem.Domain.Entities;
+namespace CabManagementSystem.Domain.Abstract
+{
+    public interface IVehicleBillRepository
+    {
+        IEnumerable<VehicleBill> VehicleBills { get; }
+
+        void SaveBill(VehicleBill vehicleBill);
+
+    }
+}
---
+++ CabManagementSystem.Domain/Concrete/EFVehicleBillRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabManagementSystem.Domain.Abstract;
+using CabManagementSystem.Domain.Entities;
+
+namespace CabManagementSystem.Domain.Concrete
+{
+    public class EFVehicleBillRepository : IVehicleBillRepository
+    {
+        private CabDbContext context = new CabDbContext();
+        public IEnumerable<ShiftTiming> ShiftTimings => context.ShiftTimings;
+
+        public IEnumerable<VehicleBill> VehicleBills => context.VehicleBills;
+
+        public void SaveBill(VehicleBill vehicleBill)
+        {
+            if (vehicleBill.billId == 0)
+            {
+                context.VehicleBills.Add(vehicleBill);
+            }
+            else
+            {
+                VehicleBill dbEntry = context.VehicleBills.Find(vehicleBill.billId);
+                if (dbEntry != null)
+                {
+                    dbEntry.billId = vehicleBill.billId;
+                    dbEntry.billNum = vehicleBill.billNum;
+                    dbEntry.vehicleId = vehicleBill.vehicleId;
+                    dbEntry.totalAmount = vehicleBill.totalAmount;
+                    dbEntry.dateOfBilling = vehicleBill.dateOfBilling;
+                    dbEntry.venderId = vehicleBill.vehicleId;
+                    dbEntry.deductions = vehicleBill.deductions;
+                    dbEntry.netAmount = vehicleBill.netAmount;
+                }
+            }
+            context.SaveChanges();
+        }
+
+    }
+}
