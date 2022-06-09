e74cc652dab64db4d49b9bfff5b5f07aa2dca22d
Shubhamzingare123 <s.zingare@zensar.com>
2022-06-09 18:32:57 +0530

    gv

---
+++ CabManagementSystem.Domain/Abstract/IVehicleAllocationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabManagementSystem.Domain.Entities;
+namespace CabManagementSystem.Domain.Abstract
+{
+    public interface IVehicleAllocationRepository
+    {
+        IEnumerable<VehicleAllocation> vehicleAllocations { get; }
+
+    }
+}
---
+++ CabManagementSystem.WebUI/Controllers/VehicleAllocationController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CabManagementSystem.Domain.Concrete;
+using CabManagementSystem.Domain.Entities;
+using CabManagementSystem.Domain.Abstract;
+using CabManagementSystem.WebUI.Models;
+namespace CabManagementSystem.WebUI.Controllers
+{
+    public class VehicleAllocationController : Controller
+    {
+        CabDbContext db = new CabDbContext();
+
+        public int PageSize = 3;
+
+        private IVehicleAllocationRepository repository;
+
+        public VehicleAllocationController(IVehicleAllocationRepository repo)
+        {
+                this.repository = repo;
+        }
+
+        public ViewResult VehicleAllocationList(int page = 1)
+        {
+            VehicleAllocationViewModel model = new VehicleAllocationViewModel
+            {
+                VehicleAllocations = repository.vehicleAllocations
+                 .OrderBy(p => p.vehicleId)
+                                .Skip((page - 1) * PageSize)
+                                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.vehicleAllocations.Count()
+                }
+
+            };
+            return View(model);
+        }
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(VehicleAllocation vehicleAllocation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VehicleAllocations.Add(vehicleAllocation);
+                db.SaveChanges();
+                return RedirectToAction("Create");
+            }
+
+            return View(vehicleAllocation);
+        }
+    }
+}
