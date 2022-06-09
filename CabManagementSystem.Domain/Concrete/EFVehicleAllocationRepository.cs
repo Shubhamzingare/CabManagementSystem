161fdd389bf2af76d03efbecaf87281033edd16f
Shubhamzingare123 <s.zingare@zensar.com>
2022-06-09 18:25:48 +0530

    new things

---
+++ CabManagementSystem.Domain/Concrete/EFVehicleAllocationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabManagementSystem.Domain.Abstract;
+using CabManagementSystem.Domain.Entities;
+
+namespace CabManagementSystem.Domain.Concrete
+{
+    public class EFVehicleAllocationRepository : IVehicleAllocationRepository
+    {
+        CabDbContext context = new CabDbContext();
+
+        public IEnumerable<VehicleAllocation> vehicleAllocations => context.VehicleAllocations;
+    }
+}
