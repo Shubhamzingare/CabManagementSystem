62c1ede8886b3741d5697327be580b5309ef3e8d
n-singam <n.singam@zensar.com>
2022-06-14 18:57:35 +0530

    updated user profile.Project is done

---
+++ CabManagementSystem.WebUI/Controllers/ProfileController.cs
@@ -0,0 +1,27 @@
+﻿using CabManagementSystem.Domain.Concrete;
+using CabManagementSystem.Domain.Entities;
+using CabManagementSystem.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CabManagementSystem.WebUI.Controllers
+{
+    [CustomAuthenticationFilter]
+    public class ProfileController : Controller
+    {
+        CabDbContext context = new CabDbContext();
+
+        public ActionResult ViewProfile(User user)
+        {
+            var username = Convert.ToString(Session["UserName"]);
+            var userdata = context.Users.Where(p => p.UserName == username).ToList();
+
+            return View(userdata);
+        }
+        
+    }
+}
